eae4597fbd67b8dc39f2077450fae5a108cf9ded
dokyung9727 <dodok9727@naver.com>
2022-10-11 16:10:22 +0900

    p5 예제 수정2

--- sketch.js
+++ sketch.js
@@ -1,10 +1,7 @@
 function setup() {
-    createCanvas(300, 300);
-    strokeWeight(5);
-    background(50); 
-  }
-  
-  function draw() {
-    stroke(random(30), random(30), random(255),random(255));
-    line(random(width), random(height), random(width), random(height));
-  }
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
